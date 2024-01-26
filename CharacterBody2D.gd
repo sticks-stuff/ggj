6fe16dff6c7357bd0330725c032dbd3cad266ebc
the-pixelgrapher <pixelgrapher@outlook.co.nz>
2024-01-27 12:17:06 +1300

    Set up player movement + mouse look

---
+++ CharacterBody2D.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
