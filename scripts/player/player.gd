3562e3c2946ef443d540ed6950c18c25d8c3ed5c
charlotte <charlottemackenzi@gmail.com>
2024-01-27 17:22:08 +1300

    smooth player velocity

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -8,7 +8,7 @@ var target_velocity = Vector3.ZERO
 var direction = Vector3.ZERO
 
 func _physics_process(delta):
-
+	var curSpeed = speed
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 	if Input.is_action_pressed("left"):
@@ -18,21 +18,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("up"):
 		direction.z -= 1
 	if not Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("down") and not Input.is_action_pressed("up"): # this sucks
-		speed = 0
+		curSpeed = 0
 	else:
-		speed = 14
+		curSpeed = speed
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
 		look_at(global_transform.origin + direction, Vector3.UP)
 
 	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity.x = direction.x * curSpeed
+	target_velocity.z = direction.z * curSpeed
 	
+	# Smooth the velocity
+	velocity.x = lerp(velocity.x, target_velocity.x, (delta * 10)) # magic number 10 :)
+	velocity.z = lerp(velocity.z, target_velocity.z, (delta * 10))
+
 	# Moving the Character
-	velocity = target_velocity
 	move_and_slide()
 
 #### FIRE
