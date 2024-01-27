d0834f460463a330f058c2c0b59a9f850badeb2c
sharlot <4157860+sticks-stuff@users.noreply.github.com>
2024-01-27 17:33:50 +1300

    Improve player movement and bullet speed (#14)
    * smooth turning
    
    * smooth player velocity
    
    * lerp bullet speed
    pass player speed to bullet

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -5,9 +5,10 @@ extends CharacterBody3D
 
 var target_velocity = Vector3.ZERO
 
-func _physics_process(delta):
-	var direction = Vector3.ZERO
+var direction = Vector3.ZERO
 
+func _physics_process(delta):
+	var curSpeed = speed
 	if Input.is_action_pressed("right"):
 		direction.x += 1
 	if Input.is_action_pressed("left"):
@@ -16,18 +17,24 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("up"):
 		direction.z -= 1
+	if not Input.is_action_pressed("right") and not Input.is_action_pressed("left") and not Input.is_action_pressed("down") and not Input.is_action_pressed("up"): # this sucks
+		curSpeed = 0
+	else:
+		curSpeed = speed
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		#$Pivot.basis = Basis.looking_at(direction)
 		look_at(global_transform.origin + direction, Vector3.UP)
 
 	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	target_velocity.x = direction.x * curSpeed
+	target_velocity.z = direction.z * curSpeed
 	
+	# Smooth the velocity
+	velocity.x = lerp(velocity.x, target_velocity.x, (delta * 10)) # magic number 10 :)
+	velocity.z = lerp(velocity.z, target_velocity.z, (delta * 10))
+
 	# Moving the Character
-	velocity = target_velocity
 	move_and_slide()
 
 #### FIRE
