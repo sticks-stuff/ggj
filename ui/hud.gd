36305345c897c5b019848a03e2659c9880e7fa77
Pixel <38391367+the-pixelgrapher@users.noreply.github.com>
2024-01-28 16:16:32 +1300

    Death screen reload fix (#31)
    * Death screen appears when player HP reaches 0
    
    * Play again button returns to start scene
    
    * Fix issue with reloading game

--- ui/hud.gd
+++ ui/hud.gd
@@ -2,11 +2,13 @@ extends Control
 
 @export var healthbar: Range
 @export var boss_healthbar: Range
+@export var death_screen: Control
+@export var playagain_button: BaseButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	playagain_button.pressed.connect(self._play_again)
+	death_screen.visible = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,6 +16,20 @@ func _process(delta):
 	
 func update_player_hp(v):
 	healthbar.value = v
+	if v <= 0:
+		show_death_screen()
 	
 func update_boss_hp(v):
 	boss_healthbar.value = v
+	
+func  show_death_screen():
+	death_screen.visible = true;
+	
+func _play_again():
+	var root_node = get_tree().get_root()
+	var scene_node = root_node.get_node("Node3D")
+	scene_node.queue_free()
+	
+	var new_scene_resource = load("res://scenes/player_draw_pane.tscn") # Load the new level from disk
+	var new_scene_node = new_scene_resource.instantiate() # Create an actual node of it for the game to use
+	root_node.add_child(new_scene_node) # Add to the tree so the level starts processing
