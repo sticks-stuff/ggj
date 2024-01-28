aa03cdae8d84725365605d2965f7c081f9d4e4e5
the-pixelgrapher <pixelgrapher@outlook.co.nz>
2024-01-28 16:10:13 +1300

    Fix issue with reloading game

--- ui/hud.gd
+++ ui/hud.gd
@@ -26,5 +26,10 @@ func  show_death_screen():
 	death_screen.visible = true;
 	
 func _play_again():
-	#var startscene = preload("res://scenes/player_draw_pane.tscn").instantiate();
-	get_tree().change_scene_to_file("res://scenes/player_draw_pane.tscn")
+	var root_node = get_tree().get_root()
+	var scene_node = root_node.get_node("Node3D")
+	scene_node.queue_free()
+	
+	var new_scene_resource = load("res://scenes/player_draw_pane.tscn") # Load the new level from disk
+	var new_scene_node = new_scene_resource.instantiate() # Create an actual node of it for the game to use
+	root_node.add_child(new_scene_node) # Add to the tree so the level starts processing
