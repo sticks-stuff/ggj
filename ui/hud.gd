4d8bdf515832710e5696714f03c1bc5aa761c229
the-pixelgrapher <pixelgrapher@outlook.co.nz>
2024-01-28 15:38:34 +1300

    Play again button returns to start scene

--- ui/hud.gd
+++ ui/hud.gd
@@ -3,11 +3,12 @@ extends Control
 @export var healthbar: Range
 @export var boss_healthbar: Range
 @export var death_screen: Control
+@export var playagain_button: BaseButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	playagain_button.pressed.connect(self._play_again)
+	death_screen.visible = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,3 +24,7 @@ func update_boss_hp(v):
 	
 func  show_death_screen():
 	death_screen.visible = true;
+	
+func _play_again():
+	#var startscene = preload("res://scenes/player_draw_pane.tscn").instantiate();
+	get_tree().change_scene_to_file("res://scenes/player_draw_pane.tscn")
